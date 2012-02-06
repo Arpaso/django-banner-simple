af86e7cf7181e05b9b0e8265bc848d5ccc1c8ea2
Arvid Paeglit <arvid@arpaso.com>
2012-02-06 09:50:32 +0400

    removed context from template tag; renamed media folder

--- src/banner/templatetags/banner_tags.py
+++ src/banner/templatetags/banner_tags.py
@@ -5,8 +5,12 @@ from native_tags.decorators import function
 
 from ..models import Banner
 
-def get_banner(context, slug, width, height, queryset=Banner.objects.all()):
+def get_banner(slug, width, height, queryset=Banner.objects.all()):
     banner = queryset.get_or_create(slug=slug.encode('utf-8'))[0]
-    return "banner_source.html", {'banner': banner, 'size': "%ix%i" % (width, height), 
-                                  'width': width, 'height': height, 'STATIC_URL': settings.STATIC_URL}
-get_banner = function(get_banner, inclusion=True, cache=60, takes_context=True)
+    return "banner_source.html", {
+                'banner': banner, 'size': "%ix%i" % (width, height), 
+                'width': width, 'height': height, 
+                'STATIC_URL': settings.STATIC_URL
+    }
+
+get_banner = function(get_banner, inclusion=True, cache=60)
